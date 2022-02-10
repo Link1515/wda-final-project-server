98e71e950bf45f01a1352a70a5950948a43542aa
Link1515 <h26281660@gmail.com>
2022-02-10 14:37:29 +0800

    socket io 連線創建 room

--- socketControllers/socket.js
+++ socketControllers/socket.js
@@ -1,21 +1,27 @@
-import { nanoid } from 'nanoid'
+import { customAlphabet } from 'nanoid'
 
+const nanoid = customAlphabet('0123456789', 6)
 const activeRooms = []
 
 export default (io) => {
   return (socket) => {
+    console.log('連線' + socket.id)
     socket.on('createRoom', ({ playerName, playerAmount, gameId }) => {
+      const roomId = nanoid()
       socket.playerName = playerName
-      const roomId = nanoid(6)
+      socket.roomId = roomId
+
       socket.join(roomId)
+
       activeRooms.push({ roomId, playerAmount, gameId, playerList: [{ name: playerName, ready: false }] })
-      socket.emit('roomSetting', { roomId, playerAmount })
-      console.log(io.sockets.adapter.rooms.get(roomId).size)
+      socket.emit('joinRoomSuccess', { roomId, playerAmount })
     })
 
-    socket.on('joinRoom', (roomId) => {
+    socket.on('joinRoom', ({ roomId, playerName }) => {
       let hasRoom = false
       let playerAmount = ''
+      socket.playerName = playerName
+      socket.roomId = roomId
 
       for (const room of activeRooms) {
         if (room.roomId === roomId) {
@@ -30,8 +36,18 @@ export default (io) => {
         return
       }
 
+      if (io.sockets.adapter.rooms.get(roomId).size >= playerAmount) {
+        socket.emit('error', '遊戲間已滿')
+        return
+      }
+
       socket.join(roomId)
-      socket.emit('roomSetting', { roomId, playerAmount })
+      socket.emit('joinRoomSuccess', { roomId, playerAmount })
+      io.to(roomId).emit('updateRoomData', { joinedPlayerAmount: io.sockets.adapter.rooms.get(roomId).size })
+    })
+
+    socket.on('disconnect', () => {
+      console.log('斷線')
     })
   }
 }
