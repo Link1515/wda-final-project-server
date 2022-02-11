915ed85223d3ce434ad8d3abe97665e05a9e7d61
Link1515 <h26281660@gmail.com>
2022-02-11 16:25:09 +0800

    修改 socket 屬性

--- socketControllers/socket.js
+++ socketControllers/socket.js
@@ -1,7 +1,7 @@
 import { customAlphabet } from 'nanoid'
 
 const nanoid = customAlphabet('0123456789', 6)
-const activeRooms = []
+let activeRooms = []
 
 export default (io) => {
   return (socket) => {
@@ -9,20 +9,20 @@ export default (io) => {
 
     socket.on('createRoom', ({ playerId, playerName, playerAmount, gameId }) => {
       const roomId = nanoid()
-      socket.playerId = playerId
-      socket.playerName = playerName
+
       socket.roomId = roomId
 
       socket.join(roomId)
 
+      const creatorInfo = { role: 1, socketId: socket.id, playerId, name: playerName, ready: false }
       activeRooms.push({
         roomId,
         playerAmount,
         gameId,
-        playerList: [{ id: playerId, name: playerName, ready: false }]
+        playerList: [creatorInfo]
       })
       socket.emit('joinRoomSuccess', { roomId, playerAmount })
-      socket.emit('updateRoomData', { joinedPlayerAmount: 1, playerList: [{ id: playerId, name: playerName, ready: false }] })
+      socket.emit('updateRoomData', { joinedPlayerAmount: 1, playerList: [creatorInfo] })
     })
 
     socket.on('joinRoom', ({ playerId, playerName, roomId }) => {
@@ -49,28 +49,48 @@ export default (io) => {
       }
 
       for (const player of currentRoom.playerList) {
-        if (playerId === player.id) {
+        if (playerId === player.playerId) {
           socket.emit('error', '帳戶於其他瀏覽器使用中')
           return
         }
       }
 
-      socket.playerId = playerId
-      socket.playerName = playerName
       socket.roomId = roomId
 
       socket.join(roomId)
       socket.emit('joinRoomSuccess', { roomId, playerAmount })
-      currentRoom.playerList.push({ id: playerId, name: playerName, ready: false })
+      currentRoom.playerList.push({ role: 0, socketId: socket.id, playerId, name: playerName, ready: false })
       io.to(roomId).emit('updateRoomData', { joinedPlayerAmount: io.sockets.adapter.rooms.get(roomId).size, playerList: currentRoom.playerList })
+    })
+
+    socket.on('toggleReady', () => {
+      const currentRoom = activeRooms.filter(room => room.roomId === socket.roomId)[0]
+      if (currentRoom) {
+        const playerIndex = currentRoom.playerList.findIndex(player => player.socketId === socket.id)
+
+        currentRoom.playerList[playerIndex].ready = !currentRoom.playerList[playerIndex].ready
+        io.to(socket.roomId).emit('updateRoomData', { joinedPlayerAmount: io.sockets.adapter.rooms.get(socket.roomId).size, playerList: currentRoom.playerList })
+      }
     })
 
     socket.on('disconnect', () => {
       if (socket.roomId) {
         const currentRoom = activeRooms.filter(room => room.roomId === socket.roomId)[0]
-        currentRoom.playerList = currentRoom.playerList.filter(player => player.id !== socket.playerId)
-        io.to(socket.roomId).emit('updateRoomData', { joinedPlayerAmount: io.sockets.adapter.rooms.get(socket.roomId).size, playerList: currentRoom.playerList })
+        const leavingPlayer = currentRoom.playerList.filter(player => player.socketId === socket.id)[0]
+
+        if (leavingPlayer.role === 1) {
+          currentRoom.playerList[1].role = 1
+        }
+
+        currentRoom.playerList = currentRoom.playerList.filter(player => player.socketId !== socket.id)
+
+        if (io.sockets.adapter.rooms.get(socket.roomId)) {
+          io.to(socket.roomId).emit('updateRoomData', { joinedPlayerAmount: io.sockets.adapter.rooms.get(socket.roomId).size, playerList: currentRoom.playerList })
+        } else {
+          activeRooms = activeRooms.filter(room => room.roomId !== socket.roomId)
+        }
       }
+
       console.log('斷線')
     })
   }
