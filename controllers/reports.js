622ff14c91a6279baf328059de45335e7c9a96e6
Link1515 <h26281660@gmail.com>
2022-02-20 17:29:48 +0800

    reports 路由

--- controllers/reports.js
+++ controllers/reports.js
@@ -16,3 +16,25 @@ export const createReport = async (req, res) => {
     }
   }
 }
+
+export const getReports = async (req, res) => {
+  try {
+    const result = await reports.find({})
+    if (result.length > 0) {
+      res.status(200).send({ success: true, message: '', result })
+    } else {
+      res.status(404).send({ success: false, message: '目前無錯誤回報' })
+    }
+  } catch (error) {
+    res.status(500).send({ success: false, message: '伺服器錯誤' })
+  }
+}
+
+export const deleteReport = async (req, res) => {
+  try {
+    await reports.findByIdAndDelete(req.params.id)
+    res.status(200).send({ success: true, message: '' })
+  } catch (error) {
+    res.status(500).send({ success: false, message: '伺服器錯誤' })
+  }
+}
--- routers/reports.js
+++ routers/reports.js
@@ -1,9 +1,12 @@
 import express from 'express'
+import { createReport, getReports, deleteReport } from '../controllers/reports.js'
 import auth from '../middleware/auth.js'
-import { createReport } from '../controllers/reports.js'
+import isAdmin from '../middleware/isAdmin.js'
 
 const Router = express.Router()
 
 Router.post('/createReport', auth, createReport)
+Router.get('/getReports', auth, isAdmin, getReports)
+Router.delete('/deleteReport/:id', auth, isAdmin, deleteReport)
 
 export default Router
